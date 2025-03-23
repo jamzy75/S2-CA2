b3751464f7ad1f827e23ecef1386ee32d95ca312
Filip Vojtěch <26953790+FilipVojtech@users.noreply.github.com>
2025-03-23 23:31:35 +0000

    Use review VM in create action

--- S2-CA2/Controllers/ReviewsController.cs
+++ S2-CA2/Controllers/ReviewsController.cs
@@ -4,8 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using S2_CA2.Data;
 using S2_CA2.Models;
-using System.Linq;
-using System.Threading.Tasks;
+using S2_CA2.Models.ViewModels;
 
 namespace S2_CA2.Controllers
 {
@@ -32,14 +31,30 @@ namespace S2_CA2.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(Review review)
+        public async Task<IActionResult> Create(ReviewVM review)
         {
+            var user = await _userManager.GetUserAsync(User);
+            var book = await _context.Books.FindAsync(review.BookId);
+            if (book == null)
+            {
+                ModelState.AddModelError(nameof(ReviewVM.BookId), "Could not find the specified book");
+                return View(review);
+            }
+
             if (ModelState.IsValid)
             {
-                _context.Reviews.Add(review);
+                _context.Reviews.Add(new Review
+                {
+                    Book = book,
+                    Content = review.Content,
+                    Rating = review.Rating,
+                    CreatedAt = DateTime.Now,
+                    User = user!
+                });
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
+
             return View(review);
         }
     }
